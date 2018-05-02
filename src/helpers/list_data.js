f44d11eeff284824e63ce4d6976fa9fcc0f9e9d2
Dona Anda <ceka_dona@yahoo.com>
2018-05-02 16:24:17 -0700

    Completed displaying dummy data

---
+++ src/helpers/list_data.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'Learn React',
+        details: 'Mearn reactJS'
+    },
+    {
+        title: 'code',
+        details: 'Python'
+    },
+    {
+        title: 'home',
+        details: 'going'
+    },
+    {
+        title: 'csgo',
+        details: 'play'
+    }
+];
